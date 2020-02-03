75918657cb9bd9c0eb40aa1fc600d70d7cf80443
Zaire  Swailes <swaileszaire@gmail.com>
2020-02-03 01:39:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand, :cobble
+end 
+
+  def initialize(brand = "Nike", color = "Red", size = "9.5", material = "suede", condition = "tattered")
+end
+
+def cobble
+$stdout 
+puts "your shoe is as good as new!"
+end
+end
